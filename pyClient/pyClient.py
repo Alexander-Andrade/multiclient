dc747526fe40b4351d762189491acd4cdca691ff
Alexander-Andrade <veimar.94@gmail.com>
2015-11-20 13:52:34 +0300

    main

--- pyClient/pyClient.py
+++ pyClient/pyClient.py
@@ -6,13 +6,14 @@ from Connection import*
 
 class Client(Connection):
 
-    def __init__(self,IP,port, sendBufLen, timeOut):
+    def __init__(self,IP,port, sendBufLen=2048, timeOut=30):
         return super().__init__(sendBufLen, timeOut)
         self.IP = IP
         self.port = port
         self.sock = None
-    def __createClient(self,IP,port):
 
+
+    def __createClient(self,IP,port):
         for addrInfo in socket.getaddrinfo(IP,port,socket.AF_UNSPEC,socket.SOCK_STREAM):
             af_family,socktype,proto,canonname,sockaddr = addrInfo
 
@@ -40,19 +41,7 @@ class Client(Connection):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     
 
-    server = TCPServer(sys.argv[1],sys.argv[2])
+    client = Client(sys.argv[1],sys.argv[2])
