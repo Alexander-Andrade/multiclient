07214890abb7729979ea675ad9b46c458b803c8a
Alexander-Andrade <veimar.94@gmail.com>
2016-01-05 22:52:50 +0300

    qwer

--- pyClient/pyClient.py
+++ pyClient/pyClient.py
@@ -1,67 +1,83 @@
 ﻿import sys  #for IP and port passing
 import socket
 import re   #regular expressions
-from Connection import Connection
 from SocketWrapper import*
 from FileWorker import*
-from random import randint
-import time
 
-class Client(Connection):
+class QueryError(Exception):
+    pass
 
-    def __init__(self,IP,port, sendBufLen=1024, timeOut=15):
-        super().__init__(sendBufLen, timeOut)
-        self.sock = TCP_ClientSockWrapper(IP,port,createId=True)
-        #send socket id
-        #self.sock.sendInt(self.sock.id)
-        self.udpSock = UDP_ClientSockWrapper(IP,port)
+class Client:
+
+    def __init__(self,IP,ports):
+        self.IP = IP
+        self.ports = ports
+        self.sock = None 
         #fill dictionary with all available commands
         self.__fillCommandDict()
 
  
     def __fillCommandDict(self):
-        self.commands.update({'download':self.recvFileTCP,
-                              'upload':self.sendFileTCP,
-                              'download_udp':self.recvFileUDP,
-                              'upload_udp':self.sendFileUDP})
+        self.commands = ({'time':(self.ports[0],None),
+                       'echo':(self.ports[1],None), 
+                       'download':(self.ports[2],self.recvFileTCP)})
 
+    def sendfile(self,sock,commandArgs,recoveryFunc):
+        try:
+            fileWorker = FileWorker(sock,commandArgs,recoveryFunc)
+            fileWorker.sendFileInfo()
+            fileWorker.sendPacketsTCP()
+        except FileWorkerError:
+            pass 
+       
+    def receivefile(self,sock,commandArgs,recoveryFunc):
+        try:
+            fileWorker = FileWorker(sock,commandArgs,recoveryFunc)
+            fileWorker.recvFileInfo()
+            fileWorker.recvPacketsTCP()
+        except FileWorkerError:
+            pass
 
     def sendFileTCP(self,commandArgs):
-        self.sendfile(self.sock,commandArgs,self.recoverTCP)
+        self.sendfile(self.sock,commandArgs,None)
 
     def recvFileTCP(self,commandArgs):
-        self.receivefile(self.sock,commandArgs,self.recoverTCP)
+        self.receivefile(self.sock,commandArgs,None)
 
-    def recoverTCP(self,timeOut):
-        start = time.time()
-        timediff = 0
-        while(True):
-            timediff = time.time() - start
-            if timediff > timeOut:
-                raise OSError("reconnection timeout")
-            if self.sock.reattachClientSock():
-                #send client id to server
-                self.sock.sendInt(self.sock.id)
-                return self.sock
-  
-
-    def sendFileUDP(self,commandArgs):
-        self.udpSock.sendInt(1)
-        self.sendfile(self.udpSock,commandArgs,self.recoverTCP)
-
-    def recvFileUDP(self,commandArgs):
-        self.udpSock.sendInt(1)
-        self.recvfileUDP(self.udpSock,commandArgs)
+    def parseCommand(self,cmd_msg):
+        commandRegEx = re.compile("[A-Za-z0-9_]+")
+        #match() Determine if the RE matches at the beginning of the string.
+        matchObj = commandRegEx.match(cmd_msg)
+        if(matchObj == None):
+            #there is no suitable command
+            return False
+        #group()	Return the string matched by the RE
+        str_cmd = matchObj.group()
+        if not str_cmd in self.commands:
+            raise QueryError('command is not implemented')
+        #end() Return the ending position of the match
+        commandEndPos = matchObj.end()
+        #cut finding command from the commandMes
+        args = cmd_msg[commandEndPos:]
+        #cut spaces after command
+        args = args.lstrip()
+        return (str_cmd,args)
+      
 
     def workingWithServer(self):
         while True:
             try:
-                commandMsg = input('->')
-                self.sock.sendMsg(commandMsg)
-                self.catchCommand(commandMsg)
+                cmd_msg = input('->')
+                str_cmd,args = self.parseCommand(cmd_msg)
+                #create socket
+                port,routine = self.commands[str_cmd]
+                self.sock = TCP_ClientSockWrapper(self.IP,port)
+                self.sock.sendMsg(cmd_msg)
+                if routine:
+                    routine(args)
                 print(self.sock.recvMsg())
 
-            except FileWorkerError as e:
+            except (QueryError) as e:
                 print(e)
                 continue
             except (OSError,FileWorkerError):
@@ -69,5 +85,5 @@ class Client(Connection):
        
 
 if __name__ == "__main__":
-    client = Client(sys.argv[1],sys.argv[2])
+    client = Client(sys.argv[1],sys.argv[2:])
     client.workingWithServer()
